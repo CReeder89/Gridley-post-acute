e96b584a578bc9535d0f068d84ad9ead558b3419
SolStonez <103968041+SolStonez@users.noreply.github.com>
2024-10-24 11:02:24 -0700

    testing rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Head from 'next/head';
+//import Head from 'next/head';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
