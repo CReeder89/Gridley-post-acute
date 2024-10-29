6330e2622eadac811d65b2f5ff9ae5d800529df0
SolStonez <103968041+SolStonez@users.noreply.github.com>
2024-10-29 11:59:33 -0700

    reverting back

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,6 @@ const Home: React.FC = () => {
             width={400}
             height={250}
             style={facilityImageStyle}
-            layout="intrinsic"
           />
           <h3>Gridley Post Acute</h3>
           <p>
@@ -78,7 +77,6 @@ const Home: React.FC = () => {
             alt="Gridley Logo"
             width={200}
             height={80}
-            layout="intrinsic"
           />
         </div>
         
@@ -89,7 +87,6 @@ const Home: React.FC = () => {
             alt="GPA Logo"
             width={400}
             height={137}
-            layout="intrinsic"
           />
           <br />
           <Link href='/test'><h3>Why Choose Us?</h3></Link>
@@ -103,7 +100,6 @@ const Home: React.FC = () => {
             alt="CMS Logo"
             width={420}
             height={242}
-            layout="intrinsic"
           />
           <p><strong>Butte County's only Five-Star Facility</strong></p>
          
