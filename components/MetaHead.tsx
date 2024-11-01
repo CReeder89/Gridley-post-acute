fea89eae98f43049c13059632ac131cf4e41ff7a
SolStonez <103968041+SolStonez@users.noreply.github.com>
2024-11-01 13:00:24 -0700

    Update MetaHead.tsx

--- components/MetaHead.tsx
+++ components/MetaHead.tsx
@@ -15,7 +15,7 @@ const MetaHead: React.FC<ButtonProps> = ({ title, description, image, url }) => 
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="keywords" content="skilled nursing, rehabilitation, Gridley, post-acute care, Butte County, 5-star nursing facility" />
-        <meta property="og:title" content="Gridley Post Acute | Skilled Nursing & Rehabilitation" />
+        <meta property="og:title" content={title} />
         <meta property="og:description" content="Discover our top-rated skilled nursing and rehabilitation services in Butte County. Gridley Post Acute is committed to exceptional care." />
         <meta property="og:image" content={image} /> {/* Replace with your image */}
         <meta property="og:url" content= {url} /> {/* Replace with your website URL */}
